ba414241846ae61a9dd387a76203eb98c95d79c6
Pikku-a <aapo1mattila@gmail.com>
2021-01-29 00:55:36 +0200

    first commit

---
+++ remove-comments.js
@@ -0,0 +1,17 @@
+document.getElementById("_bottomDisplay").style.display = "none";
+document.getElementById("challengeRoundUp").style.display = "none";
+
+/*var btn = document.createElement("BUTTON"); 
+//var btn = document.createElement("INPUT");
+//btn.setAttribute("type","submit");
+btn.setAttribute('onclick', 'loadComments()');
+btn.innerHTML="Load Comments";
+//btn.value="Load Comments";
+document.getElementById("footer").appendChild(btn);
+
+function loadComments() {
+	document.getElementById("_bottomDisplay").style.display = "block";
+	document.getElementById("challengeRoundUp").style.display = "block";
+}
+
+//getElementsByClassName("challenge_spot")
