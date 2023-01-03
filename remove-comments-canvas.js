c73fd07047b1f442b933f0522a9d3d62c30c48c2
Aapo <aapo1mattila@gmail.com>
2023-01-03 19:52:24 +0200

    fixes and moved image back

--- remove-comments-canvas.js
+++ remove-comments-canvas.js
@@ -2,6 +2,11 @@
 //document.getElementById("challengeRoundUp").style.display = "none";
 
 document.getElementById("cbox_module").style.display = "none";
+document.getElementById("newTitleRanking").style.display = "none";
 document.getElementById("genreRanking").style.display = "none";
+document.getElementById("rateRanking").style.display = "none";
+document.getElementById("challengeGenreRanking").style.display = "none";
+
+/*document.getElementById("genreRanking").style.display = "none";
 document.getElementById("rateRanking").style.display = "none";
 document.getElementById("newTitleRanking").style.display = "none";
