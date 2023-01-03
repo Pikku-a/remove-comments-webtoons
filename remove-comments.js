3f2e16ffc812f1b5f076c337a3a3a7b077ece28f
Aapo <aapo1mattila@gmail.com>
2023-01-03 20:04:38 +0200

    fixed some recommendations showing in canvas

--- remove-comments.js
+++ remove-comments.js
@@ -1,7 +1,6 @@
-//document.getElementById("_bottomDisplay").style.display = "none";
-//document.getElementById("challengeRoundUp").style.display = "none";
-
 document.getElementById("cbox_module").style.display = "none";
 document.getElementById("newTitleRanking").style.display = "none";
 document.getElementById("genreRanking").style.display = "none";
 document.getElementById("rateRanking").style.display = "none";
+//document.getElementById("_bottomDisplay").style.display = "none";
+//document.getElementById("challengeRoundUp").style.display = "none";
